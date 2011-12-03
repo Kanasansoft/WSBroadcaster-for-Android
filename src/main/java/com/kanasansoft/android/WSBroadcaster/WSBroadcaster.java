af9e49f936b0e2f946579756d49b23b056d9b45f
Kanasansoft <kanasansoft@gmail.com>
2011-12-04 00:09:05 +0900

    mavenのプラグインで作成したandroidプロジェクトを修正したものを追加。

---
+++ src/main/java/com/kanasansoft/android/WSBroadcaster/WSBroadcaster.java
@@ -0,0 +1,15 @@
+package com.kanasansoft.android.WSBroadcaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class WSBroadcaster extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
