ab8a83d8abe469f9f08913f7f364e958642e643c
Kanasansoft <kanasansoft@gmail.com>
2011-12-18 01:08:35 +0900

    とりあえず、WebSocketServerを起動できるようにした。

--- src/main/java/com/kanasansoft/android/WSBroadcaster/WSBroadcaster.java
+++ src/main/java/com/kanasansoft/android/WSBroadcaster/WSBroadcaster.java
@@ -1,5 +1,11 @@
 package com.kanasansoft.android.WSBroadcaster;
 
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +16,28 @@ public class WSBroadcaster extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		startWebSocketServer();
+	}
+
+	void startWebSocketServer() {
+
+		Server server = new Server(40320);
+
+		MyWebSocketServlet wss = new MyWebSocketServlet();
+		ServletHolder sh = new ServletHolder(wss);
+		ServletContextHandler sch = new ServletContextHandler();
+		sch.addServlet(sh, "/*");
+
+		HandlerList hl = new HandlerList();
+		hl.setHandlers(new Handler[] {sch});
+		server.setHandler(hl);
+
+		try {
+			server.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
