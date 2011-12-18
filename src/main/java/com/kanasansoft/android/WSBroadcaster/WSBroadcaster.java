a17fe7cf300fb7a71dea7a574902f60ad295904a
Kanasansoft <kanasansoft@gmail.com>
2011-12-18 15:46:14 +0900

    設定内容を画面に表示する処理を追加。

--- src/main/java/com/kanasansoft/android/WSBroadcaster/WSBroadcaster.java
+++ src/main/java/com/kanasansoft/android/WSBroadcaster/WSBroadcaster.java
@@ -8,11 +8,14 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class WSBroadcaster extends Activity {
 
@@ -41,6 +44,42 @@ public class WSBroadcaster extends Activity {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	void display() {
+
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+		String prefKeyPortNumber              = getString(R.string.preference_key_port_number);
+		String prefKeyResponseType            = getString(R.string.preference_key_response_type);
+		String prefKeyPeriodicMessage         = getString(R.string.preference_key_periodic_message);
+		String prefKeyPeriodicMessageInterval = getString(R.string.preference_key_periodic_message_interval);
+		String prefKeyPeriodicMessageText     = getString(R.string.preference_key_periodic_message_text);
+
+		String  defaultValuePortNumber              =                 getString(R.string.default_value_port_number);
+		String  defaultValueResponseType            =                 getString(R.string.default_value_response_type);
+		boolean defaultValuePeriodicMessage         = Boolean.valueOf(getString(R.string.default_value_periodic_message));
+		String  defaultValuePeriodicMessageInterval =                 getString(R.string.default_value_periodic_message_interval);
+		String  defaultValuePeriodicMessageText     =                 getString(R.string.default_value_periodic_message_text);
+
+		String currentValuePortNumber              = pref.getString (prefKeyPortNumber,              defaultValuePortNumber);
+		String currentValueResponseType            = pref.getString (prefKeyResponseType,            defaultValueResponseType);
+		String currentValuePeriodicMessage         = pref.getBoolean(prefKeyPeriodicMessage,         defaultValuePeriodicMessage)?"ON":"OFF";
+		String currentValuePeriodicMessageInterval = pref.getString (prefKeyPeriodicMessageInterval, defaultValuePeriodicMessageInterval);
+		String currentValuePeriodicMessageText     = pref.getString (prefKeyPeriodicMessageText,     defaultValuePeriodicMessageText);
+
+		TextView displayAreaPortNumber              = (TextView)findViewById(R.id.display_area_port_number);
+		TextView displayAreaResponseType            = (TextView)findViewById(R.id.display_area_response_type);
+		TextView displayAreaPeriodicMessage         = (TextView)findViewById(R.id.display_area_periodic_message);
+		TextView displayAreaPeriodicMessageInterval = (TextView)findViewById(R.id.display_area_periodic_message_interval);
+		TextView displayAreaPeriodicMessageText     = (TextView)findViewById(R.id.display_area_periodic_message_text);
+
+		displayAreaPortNumber.setText(currentValuePortNumber);
+		displayAreaResponseType.setText(currentValueResponseType);
+		displayAreaPeriodicMessage.setText(currentValuePeriodicMessage);
+		displayAreaPeriodicMessageInterval.setText(currentValuePeriodicMessageInterval);
+		displayAreaPeriodicMessageText.setText(currentValuePeriodicMessageText);
 
 	}
 
