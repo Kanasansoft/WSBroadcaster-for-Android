3fb495095a432382c573fe2637300465f2c92931
Kanasansoft <kanasansoft@gmail.com>
2011-12-19 22:10:06 +0900

    使用していないクラスのimport文を削除。

--- src/main/java/com/kanasansoft/android/WSBroadcaster/WSBroadcaster.java
+++ src/main/java/com/kanasansoft/android/WSBroadcaster/WSBroadcaster.java
@@ -19,7 +19,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
